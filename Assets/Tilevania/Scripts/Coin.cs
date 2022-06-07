d2c9654369a3146e76961fd9143719c5774e8e53
Pavel Kostyukovich <stupiddt2@gmail.com>
2022-06-07 22:12:09 +0300

    Initial commit

---
+++ Assets/Tilevania/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickUp;
+    [SerializeField] float coinPickUpVol = 0.2f;
+    [SerializeField] int coinsCost = 50;
+
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(coinPickUp, Camera.main.transform.position, coinPickUpVol);
+        FindObjectOfType<GameSession>().AddToScore(coinsCost);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Tilevania/Scripts/GameSession.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLifes = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions >1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText.text = playerLifes.ToString();
+        scoreText.text = score.ToString();
+    }
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+    public void ProcessPlayerDeath()
+    {
+        if (playerLifes > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    private void TakeLife()
+    {
+        playerLifes -= 1;
+        livesText.text = playerLifes.ToString();
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
