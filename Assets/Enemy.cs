f7eee35c7f812df657c23a8d2910ce137b90d557
Pavel Kostyukovich <stupiddt2@gmail.com>
2022-06-23 00:21:10 +0300

    Functionality to progress per kill and some level designing

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] GameObject[] activate;
+    [SerializeField] GameObject[] deactivate;
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
@@ -24,7 +26,15 @@ public class Enemy : MonoBehaviour
         }
     }
     private void Die()
-    {
+    {     
+       foreach (GameObject act in activate)
+       {
+            act.SetActive(true);      
+       }
+        foreach (GameObject deact in deactivate)
+        {
+            deact.SetActive(false);
+        }
         Destroy(gameObject);
     }
 }
