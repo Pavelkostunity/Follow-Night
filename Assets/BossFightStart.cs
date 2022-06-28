b363c052800fcb863d66873f989615afd5dc23b5
Pavel Kostyukovich <stupiddt2@gmail.com>
2022-06-28 18:08:27 +0300

    Added StartBossFight Trigger and come camera management

---
+++ Assets/BossFightStart.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightStart : MonoBehaviour
+{
+    Animator myanimator;
+    [SerializeField] GameObject[] activate;
+    [SerializeField] GameObject[] deactivate;
+    private void Start()
+    {
+        myanimator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        myanimator.SetBool("BossFight", true);
+        foreach(GameObject act in activate)
+        {
+            act.SetActive(true);
+        }
+        foreach (GameObject deact in deactivate)
+        {
+            deact.SetActive(false);
+        }
+    }
+}
