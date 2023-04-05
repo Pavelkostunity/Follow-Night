9e8239e047e409b5eeddfafd0f2757149db2ff7b
Pavel Kostyukovich <stupiddt2@gmail.com>
2023-04-05 20:10:43 +0300

    random commit

--- Assets/Tilevania/Scripts/BossHP.cs
+++ Assets/Tilevania/Scripts/BossHP.cs
@@ -7,15 +7,20 @@ public class BossHP : MonoBehaviour
 {
     [SerializeField] Boss boss;
     Slider lives;
+    public Gradient gradient;
+    public Image fill;
     // Start is called before the first frame update
     void Start()
     {
        lives = GetComponent<Slider>();
        lives.enabled = true;
        lives.maxValue = boss.GetHealth();
+
+       fill.color = gradient.Evaluate(1f);
     }
     void Update()
     {
         lives.value = boss.GetHealth();
+        fill.color = gradient.Evaluate(lives.normalizedValue);
     }
 }
--- Assets/Tilevania/Scripts/Healthbar.cs
+++ Assets/Tilevania/Scripts/Healthbar.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     Player player;
-    Text lives;
+    Slider lives;
+    public Gradient gradient;
+    public Image fill;
     // Start is called before the first frame update
     void Start()
     {
-        lives = GetComponent<Text>();
+        lives = GetComponent<Slider>();
         lives.enabled = true;
         player = FindObjectOfType<Player>();
+        lives.maxValue = player.GetHealth();
+        fill.color = gradient.Evaluate(1f);
     }
     void Update()
     {
-        lives.text = player.GetHealth().ToString() + " HP";
+        lives.value = player.GetHealth();
+        fill.color = gradient.Evaluate(lives.normalizedValue);
     }
 }
